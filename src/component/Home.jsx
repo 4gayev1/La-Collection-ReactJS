53ddb5082bae87e8262242cf9076c0fd64de2e51
Vahid Aghayev <aghayevvahid1@gmail.com>
2022-02-14 17:23:04 +0400

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className="hero">
       <div className="card bg-dark text-white border-0">
-        <img src="/assets/2822475_4_seoimageog_23985231-high_res-peaky-blinders-vi_1_.jpg" className="card-img" alt="Background" style={{"height" : " 700px","width":"1521px"}}/>
+        <img src="/assets/2822475_4_seoimageog_23985231-high_res-peaky-blinders-vi_1_.jpg" className="card-img" alt="Background" style={{"height" : " 700px","width":"1721"}}/>
         <div className="card-img-overlay d-flex flex-column justify-content-center">
             <div className="container">
           <h5 className="card-title display-3 fw-bolder mb-0">NEW SEASON ARRIVALS</h5>
